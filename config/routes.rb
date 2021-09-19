dca93c609bf9b3dd8a6e2aed581c8a9c3d446dc4
ryomanishida <riyoma5407@gmail.com>
2021-09-19 12:00:00 +0000

    feat:ページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "solar_system", to: "solar_systems#solar_system"
   get "solar_system_planet", to: "solar_systems#solar_system_planet"
+  get "galaxy", to: "spaces#galaxy"
+  get "galaxy_group", to: "spaces#galaxy_group"
+  get "cluster_of_galaxies", to: "spaces#cluster_of_galaxies"
+  get "supercluster", to: "spaces#supercluster"
+  get "end_of_unverse", to: "spaces#end_of_unverse"
 end
