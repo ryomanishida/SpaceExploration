cede01b0ba4311516442a2ca812835a4b4c4a2ca
ryomanishida <riyoma5407@gmail.com>
2021-09-25 06:57:33 +0000

    feat:文章追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "homes#top"
-  get "home", to: "homes#index"
+  get "question", to: "homes#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "solar_system", to: "spaces#solar_system"
   get "solar_system_planet", to: "spaces#solar_system_planet"
