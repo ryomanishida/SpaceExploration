396ed9ec78a6eacdede027f341b934dc6e215407
ryomanishida <riyoma5407@gmail.com>
2021-10-04 08:17:47 +0000

    電卓

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "question", to: "homes#index"
   get "movie", to: "homes#movie"
   get "news", to: "homes#news"
+  get "calculator", to: "homes#calculator"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "solar_system", to: "spaces#solar_system"
   get "solar_system_planet", to: "spaces#solar_system_planet"
